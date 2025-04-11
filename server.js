b97cf040c59724189bfec9a2689fe8337e91e717
Ashutosh <ashutoshkatoch988@gmail.com>
2025-04-11 15:52:47 +0530

    one

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+require('dotenv').config(); // at the top
+
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());  // Use express.json() instead of body-parser.json()
 app.use(express.urlencoded({ extended: true }));  // To handle form data
 
-const AZURE_LOGIC_APP_URL = "";
-
+const AZURE_LOGIC_APP_URL = process.env.ID;
 app.post("/submit-ticket", async (req, res) => {
     try {
         console.log("Received request body:", req.body);  // Debugging step
